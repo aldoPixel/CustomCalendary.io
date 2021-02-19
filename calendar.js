39b4d1824731f7c506c58dcfcbbc6cc9f684b3b7
iApolloBear <aldoespinosaperez1@gmail.com>
2021-02-19 10:49:37 -0600

    Date Validation

--- calendar.js
+++ calendar.js
@@ -1,29 +1,28 @@
 const id = () => "_" + Math.random().toString(36).substr(2, 9);
 
-document.addEventListener("DOMContentLoaded", () => {
-  const calendarEl = document.getElementById("calendar");
-  let customEvents = [];
-  let title = "";
+const loadCalendar = () => {
+  const calendarEl = document.getElementById("calendarOne");
   const calendar = new FullCalendar.Calendar(calendarEl, {
     initialView: "dayGridMonth",
     aspectRatio: 2,
     selectable: true,
     themeSystem: "bootstrap",
     select: ({ startStr, endStr }) => {
-      title = prompt("Inserta nombre");
-      title && title.trim().length > 0
-        ? calendar.addEvent({
-            id: id(),
-            title,
-            start: startStr,
-            end: endStr,
-          })
-        : alert("El evento debe llevar nombre");
+      calendar.addEvent({
+        id: id(),
+        start: startStr,
+        end: endStr,
+      });
     },
     eventClick: ({ event: { id } }) => {
       const selectedEvent = calendar.getEventById(id);
       selectedEvent.remove();
     },
+    selectOverlap: (event) => {
+      console.log(event);
+    },
   });
   calendar.render();
-});
+};
+
+document.addEventListener("DOMContentLoaded", loadCalendar);
